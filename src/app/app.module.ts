0883f83b992b1185e0ce98ad1247f30526591949
Balaji1988-java <balaji.shelgunte@gmail.com>
2020-05-22 12:48:23 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,49 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule  } from '@angular/core';
+import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
+import { HttpClientModule } from '@angular/common/http';
+
+import { HomeComponent } from './crud/home/home.component';
+import { DetailsComponent } from './crud/details/details.component';
+import { CreateComponent } from './crud/create/create.component';
+import { UpdateComponent } from './crud/update/update.component';
+import { Routes, RouterModule } from '@angular/router';
+
+
+const routes: Routes = [
+  { path: 'crud', redirectTo: 'crud/home', pathMatch: 'full'},
+  { path: 'crud/home', component: HomeComponent },
+  { path: 'crud/details/:productId', component: DetailsComponent },
+  { path: 'crud/create', component: CreateComponent },
+  { path: 'crud/update/:productId', component: UpdateComponent } ,
+
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserDetailsComponent,
+    HomeComponent,
+    DetailsComponent,
+    CreateComponent,
+    UpdateComponent
+    
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule ,
+    HttpClientModule,
+    ReactiveFormsModule ,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
